fae9f9b26dbbf79a1e4e54d2475b46d01af6603c
nesarix <ebonydelice@gmail.com>
2022-10-15 12:43:12 +1100

    fixes and fin?

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let modalJS = document.getElementById("modal");
 let modalTitle = document.getElementById("modal__fileTitle");
-let modalDetails = document.getElementById("modal__fileDetails");
 let btnMacpaint = document.getElementById("openModalImg1");
 let btnShopList = document.getElementById("openModalList");
 let btnTrash = document.getElementById("openModalTrash");
-//click events for my modal
-btnMacpaint.addEventListener("click", openPaintVersion);
-//window.addEventListener("click", closeModal);
+let toClearModal = document.getElementById("clearInside");
+let menuModalJS = document.getElementById("menuModal");
+let fileClickMenu = document.getElementById("fileOpen");
+let fileCloseOption = document.getElementById("fileClose");
+//click events
+//btnMacpaint.addEventListener("click", openPaintVersion);
 btnShopList.addEventListener("click", openListVersion);
 btnTrash.addEventListener("click", openTrashVersion);
+fileClickMenu.addEventListener("click", fileMenu);
+
+setInterval(clock, 1000);
+clock();
+
 function openPaintVersion() {
 	if (modalJS.style.display !== "block") {
 		modalJS.style.display = "block";
@@ -17,9 +24,30 @@ function openPaintVersion() {
 		paint.src = "./images/00_screenshot.png";
 		paint.style.width = "inherit";
 		paint.style.height = "inherit";
-		document.querySelector(".modal").appendChild(paint);
+		document.querySelector(".clearInside").appendChild(paint);
+
+		document.addEventListener(
+			"click",
+			() => {
+				modalJS.style.display = "none";
+			},
+			{ once: true, capture: true }
+		);
 	}
 }
+
+// so like the image just hecks off with this and i cant figure out why lmao
+// const openPaintVersion = () => {
+// 	if (modalJS.style.display !== "block") {
+// 		modalJS.style.display = "block";
+// 		modalTitle.innerText = "macpaint";
+// 		const paint = document.createElement("img");
+// 		paint.src = "./images/00_screenshot.png";
+// 		paint.style.width = "inherit";
+// 		paint.style.height = "inherit";
+// 	}
+// };
+btnMacpaint.addEventListener("click", openPaintVersion);
 function openListVersion() {
 	if (modalJS.style.display !== "block") {
 		modalJS.style.display = "block";
@@ -28,7 +56,7 @@ function openListVersion() {
 		unordList.className = "ul";
 		let liItems = document.createElement("li");
 		liItems.innerText = "chicken nuggets";
-		document.querySelector(".modal").appendChild(unordList);
+		document.querySelector(".clearInside").appendChild(unordList);
 		document.querySelector(".ul").appendChild(liItems);
 		liItems = document.createElement("li");
 		liItems.innerText = "lettuce";
@@ -36,6 +64,13 @@ function openListVersion() {
 		liItems = document.createElement("li");
 		liItems.innerText = "bread rolls";
 		document.querySelector(".ul").appendChild(liItems);
+		document.addEventListener(
+			"click",
+			() => {
+				modalJS.style.display = "none";
+			},
+			{ once: true, capture: true }
+		);
 	}
 }
 
@@ -50,11 +85,38 @@ function openTrashVersion() {
 		vid.style.height = "inherit";
 		vid.setAttribute("autoplay", "");
 		vid.setAttribute("loop", "");
-		document.querySelector(".modal").appendChild(vid);
+		vid.setAttribute("controls", "");
+		document.querySelector(".clearInside").appendChild(vid);
+		document.addEventListener(
+			"click",
+			() => {
+				modalJS.style.display = "none";
+			},
+			{ once: true, capture: true }
+		);
 	}
 }
-function closeModal(e) {
-	if (e.target == modalJS) {
-		modalJS.style.display = "none";
+function clock() {
+	let day = new Date();
+	let time = day.getHours() + ":" + day.getMinutes();
+	document.getElementById("clock").innerText = time;
+}
+function fileMenu() {
+	if (menuModalJS.style.display !== "block") {
+		menuModalJS.style.display = "block";
+		document.addEventListener(
+			"click",
+			() => {
+				menuModalJS.style.display = "none";
+			},
+			{ once: true, capture: true }
+		);
 	}
 }
+
+const clearModal = () => {
+	if (modalJS.style.display === "none") {
+		toClearModal.replaceChildren();
+	}
+};
+setInterval(clearModal, 500);
